04971529b2a50795e801828b317f20b090c7ba9f
danush242211ay <danush8747@gmail.com>
2026-04-22 10:09:47 +0530

    react setup

---
+++ vite-01/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1>Welcome to React Learning</h1>
+    <ol typeof='1'>
+      <li>React is javasricpt library used for frontend development to build compenent based website</li>
+      <li>Main basic to learn</li>
+      <li>arrow arrays, oops, promises , anysnc functions ,etc</li>
+      <li>import and export function</li>
+      <li>react setup</li>
+      <li>real DOM vs virutal DOM</li>
+    </ol>
+    </>
+  )
+}
+export default App
+
