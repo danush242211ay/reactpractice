3ebce2d86390869440ca662a23124913106b8657
danush242211ay <danush8747@gmail.com>
2026-05-25 18:02:41 +0530

    d structure , form handling , two way binding

--- use-state/src/App.jsx
+++ use-state/src/App.jsx
@@ -2,22 +2,19 @@ import { IndentDecrease } from 'lucide-react';
 import React, { useState } from 'react'
 
 const App = () => {
-  const [num, setNum] = useState(20)
+  const [num, setNum] = useState([10,20,30])
 
-  function increase(){
-    setNum(num+1);
-  }
-  function decrease(){
-    setNum(num-1);
+  function add(){
+    const newNum=[...num]
+    newNum.push(90)
+
+    setNum(newNum)
   }
   
   return (
     <div>
       <h1>Value of num is {num}</h1>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-
-      <h1></h1>
+      <button onClick={add}>Increase</button>
     </div>
   )
 }
