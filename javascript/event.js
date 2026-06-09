3252371f02116fb3efd51224ef50cfe16f16e850
danush242211ay <danush8747@gmail.com>
2026-06-09 17:07:38 +0530

    tofevents-> input, change, submit

---
+++ javascript/event.js
@@ -0,0 +1,24 @@
+// Event and Eventlistener
+
+//---add and remove event listener
+
+// let h1=document.querySelector("h1")
+// function click(){
+//     h1.style.color="red";
+// }
+// h1.addEventListener("dblclick",click)
+// h1.removeEventListener("dblclick",click)
+
+//---types of events
+// ->click, input, change, mouse, keypadup
+
+// let inp=document.querySelector("input")
+// inp.addEventListener("input",function(evt){
+//     console.log(evt.data)
+// })
+
+// let h1=document.querySelector("h1")
+// let sel=document.querySelector("select")
+// sel.addEventListener("change",function(vt){
+//     h1.textContent=`${vt.target.value} Selected`;
+// })
